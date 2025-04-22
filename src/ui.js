79bcb4a1fc011b90a61cd9a5575da4a2c0ae6ff2
Ben Helleman <bhellema@adobe.com>
2025-04-22 10:58:52 -0400

    styling fixes

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,4 @@
-import { html2md } from '@adobe/helix-importer';
+import { createMarkdown } from './markdown.js';
 
 // Initialize CodeMirror
 const editor = CodeMirror.fromTextArea(document.getElementById('jsonEditor'), {
@@ -12,59 +12,128 @@ const editor = CodeMirror.fromTextArea(document.getElementById('jsonEditor'), {
   lineWrapping: true
 });
 
+editor.setSize('100%', '100%');
+
+const markdownPreview = CodeMirror.fromTextArea(document.getElementById('import-markdown-source'), {
+  mode: 'markdown',
+  theme: 'monokai',
+  lineNumbers: true,
+});
+
+markdownPreview.setSize('100%', '100%');
+
+function groupModelFields(model) {
+  const fields = [];
+
+  const suffixes = ['Alt', 'MimeType', 'Type', 'Text', 'Title'];
+
+  model.fields
+    .filter((field) => field.name !== 'classes')
+    .forEach((field) => {
+      if (field.name.includes('_')) {
+        const groupName = field.name.split('_')[0];
+        const groupObj = fields.find((item) => item.name === groupName) || {
+          name: groupName,
+          fields: [],
+        };
+
+        if (!fields.includes(groupObj)) {
+          fields.push(groupObj);
+        }
+
+        const suffix = suffixes.find((s) => field.name.endsWith(s));
+        const collapsedName = field.name.substring(0, field.name.lastIndexOf(suffix));
+        const collapsedField = groupObj.fields.find((item) => item.name === collapsedName);
+
+        if (collapsedField) {
+          collapsedField.collapsed = collapsedField.collapsed || [];
+          collapsedField.collapsed.push(field);
+        } else {
+          groupObj.fields.push(field);
+        }
+      } else {
+        const suffix = suffixes.find((s) => field.name.endsWith(s));
+        const groupName = field.name.substring(0, field.name.indexOf(suffix));
+        let groupObj = fields.find((item) => item.name === groupName);
+
+        if (!groupObj) {
+          groupObj = {
+            name: field.name,
+            fields: [field],
+          };
+          fields.push(groupObj);
+        } else {
+          // find the field in the group that has a name that starts with the field.name
+          const collapsedField = groupObj.fields.find((item) => field.name.startsWith(item.name));
+          if (!collapsedField) {
+            throw new Error(`Unable to find the collapsed field for field: ${field.name}`);
+          }
+          collapsedField.collapsed = collapsedField.collapsed || [];
+          collapsedField.collapsed.push(field);
+        }
+      }
+    });
+
+  return fields;
+}
+
 // Function to generate HTML table from model data
-function generateTable(models) {
-  const model = models[0];
+function generateTable(fields, modelId) {
+
   let html = '<table>\n';
 
   // Add header row
   html += '  <tr>\n';
-  html += `    <th>${model.id}</th>\n`;
+  html += `    <th>${modelId.charAt(0).toUpperCase() + modelId.slice(1)}</th>\n`;
   html += '  </tr>\n';
 
   // Add content row
-  html += '  <tr>\n';
-  html += '    <td>\n';
-  html += '      <p>\n';
-
-  // Process each field
-  model.fields.forEach(field => {
-    switch (field.component) {
-      case 'text':
-        if (field.name.includes('title')) {
-          html += `        <h1>${field.value || ''}</h1>\n`;
-        } else if (field.name.includes('description')) {
-          html += `        <p>${field.value || ''}</p>\n`;
-        }
-        break;
-      case 'reference':
-        if (field.name.includes('image')) {
-          const altField = model.fields.find(f => f.name === `${field.name}Alt`);
-          const altText = altField?.value || 'Hero image';
-          html += `        <img loading="lazy" alt="${altText}" src="${field.value || ''}" width="1600" height="914">\n`;
-        }
-        break;
-    }
+  // for each field group, add a row  
+  fields.forEach(fieldGrouping => {
+    html += '  <tr>\n';
+    html += '    <td>\n';
+    html += renderField(fieldGrouping);
+    html += '    </td>\n';
+    html += '  </tr>\n';
   });
 
-  html += '      </p>\n';
-  html += '    </td>\n';
-  html += '  </tr>\n';
   html += '</table>';
 
   return html;
 }
 
+function renderField(fieldGrouping) {
+  let html = '';
+  let hasCollapsed = fieldGrouping.fields[0].collapsed;
+
+  if (hasCollapsed) {
+    // if the field has a name that ends in Type, then add a <h> element with the value of the field grouping field
+    if (fieldGrouping.fields[0].collapsed[0].name.endsWith('Type')) {
+      html += `<${fieldGrouping.fields[0].collapsed[0].value}>`;
+    }
+    html += `${fieldGrouping.fields[0].value}`;
+    if (fieldGrouping.fields[0].collapsed[0].name.endsWith('Type')) {
+      html += `</${fieldGrouping.fields[0].collapsed[0].value}>`;
+    }
+  } else {
+    html += `${fieldGrouping.fields[0].value}`;
+  }
+
+  return html;
+}
+
 // Function to update preview
-function updatePreview() {
+async function updatePreview() {
+  console.log('updatePreview');
   try {
     const modelData = JSON.parse(editor.getValue());
-    const html = generateTable(modelData);
-    document.getElementById('markdownPreview').innerHTML = html2md(html);
+    const fieldGroup = groupModelFields(modelData[0]);
+    const html = generateTable(fieldGroup, modelData[0].id);
+    const { content } = await createMarkdown(html);
+    markdownPreview.setValue(content);
 
   } catch (error) {
-    document.getElementById('markdownPreview').innerHTML =
-      `<div class="error">Error parsing JSON: ${error.message}</div>`;
+    markdownPreview.setValue(error.message);
   }
 }
 
@@ -78,30 +147,37 @@ editor.setValue(JSON.stringify([
     "fields": [
       {
         "component": "text",
-        "name": "hero_title",
-        "label": "Title",
+        "name": "title",
         "value": "Rock'n Roll Plush"
       },
       {
         "component": "text",
-        "name": "hero_titleType",
-        "label": "Type"
+        "name": "titleType",
+        "value": "h2"
       },
       {
         "component": "text",
-        "name": "hero_description",
-        "label": "Description"
+        "name": "description",
+        "value": "Description Goes here"
+      },
+      {
+        "component": "text",
+        "name": "description_cta",
+        "value": "Description Goes here"
+      },
+      {
+        "component": "text",
+        "name": "description_cta2",
+        "value": "Description Goes here"
       },
       {
         "component": "reference",
-        "name": "hero_image",
-        "label": "Image",
+        "name": "image",
         "value": "https://main--stini--bhellema.aem.live/media_1b50a0b66ff593398f892cc5a375cf708fac7141e.jpeg"
       },
       {
         "component": "text",
-        "name": "hero_imageAlt",
-        "label": "Alt",
+        "name": "imageAlt",
         "value": "Hero image"
       }
     ]
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/styles.css',
+          to: 'styles.css'
+        }
+      ]
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  }
+}; 
